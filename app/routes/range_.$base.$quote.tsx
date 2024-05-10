8cf6e50962e7f8720d9fd07f1484ee3baea7faf7
Jiahua Zhang <hughzhanghz@gmail.com>
2024-05-09 21:08:58 -0400

    add recharts

--- app/routes/range_.$base.$quote.tsx
+++ app/routes/range_.$base.$quote.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { Table } from 'antd';
+import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { getDailyForexSeries } from '~/.server/forex';
 import { range } from '~/lib/rang';
 import { ForexSeries } from '~/lib/type';
@@ -16,6 +17,20 @@ const Page = () => {
 
   return <div un-grid='~' un-gap='4' >
     <Table dataSource={result.rangeData} columns={result.rangeColumn} rowKey='range' pagination={false} size='small' />
+
+    <div un-grid='~' un-justify='center' >
+      <LineChart width={1200} height={400} data={[...result.values].reverse()}  >
+        <XAxis dataKey='datetime' />
+        <YAxis domain={['dataMin', 'dataMax']} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="high" stroke="green" />
+        <Line type="monotone" dataKey="low" stroke="red" />
+        {/* <Line type="monotone" dataKey="open" stroke="blue" /> */}
+        {/* <Line type="monotone" dataKey="close" stroke="blue" /> */}
+      </LineChart>
+    </div>
+
     <Table dataSource={result.values} columns={result.column} rowKey='datetime' pagination={false} size='small' />
   </div>;
 };
