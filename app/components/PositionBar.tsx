d539ff2334a221a4110675da60ab229f9b7d20c6
Jiahua Zhang <hughzhanghz@gmail.com>
2024-05-13 19:48:32 -0400

    add PositionBar

---
+++ app/components/PositionBar.tsx
@@ -0,0 +1,77 @@
+import { Tooltip } from 'antd';
+import BigNumber from 'bignumber.js';
+import { useEffect, useState } from 'react';
+
+export const PositionBar = ({ low, high, value }: { low: number, high: number, value: number; }) => {
+  const [width, setWidth] = useState(50);
+
+  useEffect(() => {
+    if (value < low) {
+      setWidth(BigNumber(low)
+        .minus(value)
+        .dividedBy(
+          BigNumber(high).minus(low)
+        ).multipliedBy(100)
+        .toNumber()
+      );
+    } else if (value > high) {
+      setWidth(BigNumber(value)
+        .minus(high)
+        .dividedBy(
+          BigNumber(high).minus(low)
+        ).multipliedBy(100)
+        .toNumber()
+      );
+    } else {
+      setWidth(BigNumber(value)
+        .minus(low)
+        .dividedBy(BigNumber(high).minus(low))
+        .multipliedBy(100)
+        .toNumber()
+      );
+    }
+  }, [low, high, value]);
+
+  if (value < low) {
+    return <div un-inline-grid='~' un-grid-flow='col' un-justify='start' un-gap='1' >
+      ⚠️{value}
+      <div un-min-w='14' un-max-w='200' un-bg='red-5' un-text='white center' style={{ width: width * 4 }} >
+        <Tooltip title={`${width}%`} >
+          {width.toFixed(2)}%
+        </Tooltip>
+      </div>
+      <span un-text='red-5' >{low}</span>
+      <div un-w='100' un-bg='green-6' ></div>
+      <span un-text='green-6' >{high}</span>
+    </div>;
+  }
+  if (value > high) {
+    return <div un-inline-grid='~' un-grid-flow='col' un-justify='start' un-gap='1' >
+      <span un-text='red-5' >{low}</span>
+      <div un-w='100' un-bg='red-5' ></div>
+      <span un-text='green-6' >{high}</span>
+      <div un-min-w='14' un-max-w='200' un-bg='green-6' un-text='white center' style={{ width: width * 4 }} >
+        <Tooltip title={`${width}%`} >
+          {width.toFixed(2)}%
+        </Tooltip>
+      </div>
+      {value}⚠️
+    </div>;
+  }
+
+  return <div un-inline-grid='~' un-grid-flow='col' un-justify='start' un-gap='1' >
+    <span un-text='red-5' >{low}</span>
+    <div un-min-w='8' un-bg='red-5' un-text='white center' style={{ width: width * 10 }} >
+      <Tooltip title={`${width}%`} >
+        {width.toFixed(2)}%
+      </Tooltip>
+    </div>
+    {value}
+    <div un-min-w='8' un-bg='green-6' un-text='white center' style={{ width: (100 - width) * 10 }} >
+      <Tooltip title={`${100 - width}%`} >
+        {(100 - width).toFixed(2)}%
+      </Tooltip>
+    </div>
+    <span un-text='green-6' >{high}</span>
+  </div>;
+};
---
+++ app/data/range/gbp-cad.ts
@@ -0,0 +1,252 @@
+export const data = {
+  "meta": {
+    "symbol": "GBP/CAD",
+    "interval": "1day",
+    "currency_base": "British Pound",
+    "currency_quote": "Canadian Dollar",
+    "type": "Physical Currency"
+  },
+  "values": [
+    {
+      "datetime": "2024-05-10",
+      "open": "1.71233",
+      "high": "1.71580",
+      "low": "1.70810",
+      "close": "1.71260",
+      "previous_close": "1.71280"
+    },
+    {
+      "datetime": "2024-05-09",
+      "open": "1.71476",
+      "high": "1.71577",
+      "low": "1.70850",
+      "close": "1.71280",
+      "previous_close": "1.71470"
+    },
+    {
+      "datetime": "2024-05-08",
+      "open": "1.71700",
+      "high": "1.71886",
+      "low": "1.71376",
+      "close": "1.71470",
+      "previous_close": "1.71690"
+    },
+    {
+      "datetime": "2024-05-07",
+      "open": "1.71670",
+      "high": "1.71997",
+      "low": "1.71430",
+      "close": "1.71690",
+      "previous_close": "1.71670"
+    },
+    {
+      "datetime": "2024-05-06",
+      "open": "1.71680",
+      "high": "1.72130",
+      "low": "1.71600",
+      "close": "1.71670",
+      "previous_close": "1.71710"
+    },
+    {
+      "datetime": "2024-05-03",
+      "open": "1.71364",
+      "high": "1.71990",
+      "low": "1.70990",
+      "close": "1.71710",
+      "previous_close": "1.71390"
+    },
+    {
+      "datetime": "2024-05-02",
+      "open": "1.72107",
+      "high": "1.72155",
+      "low": "1.71070",
+      "close": "1.71390",
+      "previous_close": "1.72100"
+    },
+    {
+      "datetime": "2024-05-01",
+      "open": "1.72098",
+      "high": "1.72182",
+      "low": "1.71720",
+      "close": "1.72100",
+      "previous_close": "1.72100"
+    },
+    {
+      "datetime": "2024-04-30",
+      "open": "1.71593",
+      "high": "1.72270",
+      "low": "1.71400",
+      "close": "1.72100",
+      "previous_close": "1.71599"
+    },
+    {
+      "datetime": "2024-04-29",
+      "open": "1.70800",
+      "high": "1.71660",
+      "low": "1.70790",
+      "close": "1.71599",
+      "previous_close": "1.70791"
+    },
+    {
+      "datetime": "2024-04-26",
+      "open": "1.70914",
+      "high": "1.71250",
+      "low": "1.70447",
+      "close": "1.70791",
+      "previous_close": "1.70900"
+    },
+    {
+      "datetime": "2024-04-25",
+      "open": "1.70784",
+      "high": "1.71351",
+      "low": "1.70249",
+      "close": "1.70900",
+      "previous_close": "1.70790"
+    },
+    {
+      "datetime": "2024-04-24",
+      "open": "1.70116",
+      "high": "1.70801",
+      "low": "1.69950",
+      "close": "1.70790",
+      "previous_close": "1.70100"
+    },
+    {
+      "datetime": "2024-04-23",
+      "open": "1.69187",
+      "high": "1.70260",
+      "low": "1.69020",
+      "close": "1.70100",
+      "previous_close": "1.69190"
+    },
+    {
+      "datetime": "2024-04-22",
+      "open": "1.70056",
+      "high": "1.70110",
+      "low": "1.68870",
+      "close": "1.69190",
+      "previous_close": "1.70070"
+    },
+    {
+      "datetime": "2024-04-19",
+      "open": "1.71210",
+      "high": "1.71660",
+      "low": "1.69990",
+      "close": "1.70070",
+      "previous_close": "1.71210"
+    },
+    {
+      "datetime": "2024-04-18",
+      "open": "1.71530",
+      "high": "1.71818",
+      "low": "1.71160",
+      "close": "1.71210",
+      "previous_close": "1.71530"
+    },
+    {
+      "datetime": "2024-04-17",
+      "open": "1.71840",
+      "high": "1.72340",
+      "low": "1.71430",
+      "close": "1.71530",
+      "previous_close": "1.71852"
+    },
+    {
+      "datetime": "2024-04-16",
+      "open": "1.71600",
+      "high": "1.72408",
+      "low": "1.71350",
+      "close": "1.71852",
+      "previous_close": "1.71600"
+    },
+    {
+      "datetime": "2024-04-15",
+      "open": "1.71470",
+      "high": "1.71710",
+      "low": "1.71258",
+      "close": "1.71600",
+      "previous_close": "1.71460"
+    },
+    {
+      "datetime": "2024-04-12",
+      "open": "1.71834",
+      "high": "1.71950",
+      "low": "1.71030",
+      "close": "1.71460",
+      "previous_close": "1.71840"
+    },
+    {
+      "datetime": "2024-04-11",
+      "open": "1.71570",
+      "high": "1.71920",
+      "low": "1.71450",
+      "close": "1.71840",
+      "previous_close": "1.71560"
+    },
+    {
+      "datetime": "2024-04-10",
+      "open": "1.72050",
+      "high": "1.72360",
+      "low": "1.71460",
+      "close": "1.71560",
+      "previous_close": "1.72049"
+    },
+    {
+      "datetime": "2024-04-09",
+      "open": "1.71741",
+      "high": "1.72400",
+      "low": "1.71730",
+      "close": "1.72049",
+      "previous_close": "1.71750"
+    },
+    {
+      "datetime": "2024-04-08",
+      "open": "1.71680",
+      "high": "1.71930",
+      "low": "1.71440",
+      "close": "1.71750",
+      "previous_close": "1.71760"
+    },
+    {
+      "datetime": "2024-04-06",
+      "open": "1.71710",
+      "high": "1.71766",
+      "low": "1.71670",
+      "close": "1.71760",
+      "previous_close": "1.71710"
+    },
+    {
+      "datetime": "2024-04-05",
+      "open": "1.71270",
+      "high": "1.71831",
+      "low": "1.71140",
+      "close": "1.71710",
+      "previous_close": "1.71280"
+    },
+    {
+      "datetime": "2024-04-04",
+      "open": "1.71177",
+      "high": "1.71350",
+      "low": "1.70759",
+      "close": "1.71280",
+      "previous_close": "1.71180"
+    },
+    {
+      "datetime": "2024-04-03",
+      "open": "1.70700",
+      "high": "1.71230",
+      "low": "1.70350",
+      "close": "1.71180",
+      "previous_close": "1.70690"
+    },
+    {
+      "datetime": "2024-04-02",
+      "open": "1.70330",
+      "high": "1.70770",
+      "low": "1.70240",
+      "close": "1.70690",
+      "previous_close": "1.70330"
+    }
+  ],
+  "status": "ok"
+};
