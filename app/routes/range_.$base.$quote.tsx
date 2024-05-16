f7f0a299c63bb8350629be739f18e0dd44c35707
Jiahua Zhang <hughzhanghz@gmail.com>
2024-05-15 22:10:45 -0400

    update range sub path

--- app/routes/range_.$base.$quote.tsx
+++ app/routes/range_.$base.$quote.tsx
@@ -1,23 +1,52 @@
-import type { LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { useLoaderData, useParams } from '@remix-run/react';
 import { Table } from 'antd';
+import { Suspense, useEffect, useState } from 'react';
 import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { getDailyForexSeries } from '~/.server/forex';
+import { PositionBar } from '~/components/PositionBar';
+import { data } from '~/data/range/gbp-cad';
 import { range } from '~/lib/rang';
-import { ForexSeries } from '~/lib/type';
+
+export const meta: MetaFunction = () => [{ title: 'Range $' }];
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const { base, quote } = params;
-  return getDailyForexSeries(`${base}/${quote}`);
+  const series = await getDailyForexSeries(`${base}/${quote}`);
+  return {
+    api: process.env.TWELVEDATA_API_KEY,
+    series
+  };
 };
 
 const Page = () => {
-  const data = useLoaderData() as ForexSeries;
-  const result = range(data);
+  const { api, series } = useLoaderData<typeof loader>();
+  const result = range(series as any);
+  const { base, quote } = useParams();
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    const socket = new WebSocket(`wss://ws.twelvedata.com/v1/quotes/price?apikey=${api}`);
+    socket.onmessage = (event) => {
+      const obj = JSON.parse(event.data);
+      if ('price' in obj) {
+        setPrice(obj['price']);
+      }
+    };
+
+    socket.onopen = () => {
+      socket.send(JSON.stringify({
+        action: 'subscribe',
+        params: { symbols: `${base}/${quote}` }
+      }));
+    };
+  }, []);
 
   return <div un-grid='~' un-gap='4' >
     <Table dataSource={result.rangeData} columns={result.rangeColumn} rowKey='range' pagination={false} size='small' />
-
+    <PositionBar low={result.rangeData[0].low} high={result.rangeData[0].high} value={price} />
+    <PositionBar low={result.rangeData[1].low} high={result.rangeData[1].high} value={price} />
+    <PositionBar low={result.rangeData[2].low} high={result.rangeData[2].high} value={price} />
     <div un-grid='~' un-justify='center' >
       <LineChart width={1200} height={400} data={[...result.values].reverse()}  >
         <XAxis dataKey='datetime' />
@@ -30,9 +59,10 @@ const Page = () => {
         {/* <Line type="monotone" dataKey="close" stroke="blue" /> */}
       </LineChart>
     </div>
-
     <Table dataSource={result.values} columns={result.column} rowKey='datetime' pagination={false} size='small' />
   </div>;
 };
 
-export default Page;
+export default () => <Suspense>
+  <Page />
+</Suspense>;
