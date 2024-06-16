dd677048334ad70953efe7adc101298ca29f5953
Jiahua Zhang <hughzhanghz@gmail.com>
2024-06-15 23:45:54 -0400

    update instrument page

--- app/routes/oanda.account.$id.instruments.tsx
+++ app/routes/oanda.account.$id.instruments.tsx
@@ -1,5 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
+import { Popover } from 'antd';
+import { Suspense } from 'react';
 import { getInstruments } from '~/.server/oanda/account';
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
@@ -9,9 +11,24 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 const Page = () => {
   const data = useLoaderData<typeof loader>();
 
-  return <pre>
-    {JSON.stringify(data, null, 2)}
-  </pre>;
+  return <div>
+    <div un-grid='~' un-grid-cols='[repeat(auto-fit,_minmax(6em,_1fr))]' un-gap='2' un-mx='6' un-my='6' >
+      {
+        data.instruments.map(instrument => <Popover content={<pre un-h='96' un-overflow-y='auto' >
+          {JSON.stringify(instrument, null, 2)}
+        </pre>} >
+          <Link to={`/oanda/instrument/${instrument.name}/candles`} > {instrument.name} </Link>
+        </Popover>
+        )
+      }
+    </div>
+
+    <div>
+      Last Trasnaction id: {data.lastTransactionID}
+    </div>
+  </div>;
 };
 
-export default Page;
+export default () => <Suspense>
+  <Page />
+</Suspense>;
