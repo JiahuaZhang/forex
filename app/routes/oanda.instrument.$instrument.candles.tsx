616276f4b7ecc3d4a66b2da62e0edef648bd91f4
Jiahua Zhang <hughzhanghz@gmail.com>
2024-06-28 21:33:10 -0400

    display volue data on y-axis

--- app/routes/oanda.instrument.$instrument.candles.tsx
+++ app/routes/oanda.instrument.$instrument.candles.tsx
@@ -1,7 +1,7 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { Suspense, useEffect } from 'react';
 import { getCandles } from '~/.server/oanda/instrument';
-import { createChart, ChartOptions, ColorType, CrosshairMode, LineStyle } from 'lightweight-charts';
+import { createChart, ChartOptions, ColorType, CrosshairMode, LineStyle, IPriceLine } from 'lightweight-charts';
 import { useLoaderData, useParams } from '@remix-run/react';
 import { Candlestick } from '~/.server/oanda/type/instrument';
 import { Currency } from '~/.server/oanda/type/primitives';
@@ -32,11 +32,11 @@ const useCandleChart = (id: string, currency: Currency, data: ReturnType<typeof 
   useEffect(() => {
     if (!document) return;
 
-    const currentLocale = window.navigator.language;
-    const myPriceFormatter = Intl.NumberFormat(currentLocale, {
-      style: 'currency',
-      currency: currency
-    }).format;
+    // const currentLocale = window.navigator.language;
+    // const myPriceFormatter = Intl.NumberFormat(currentLocale, {
+    //   style: 'currency',
+    //   currency
+    // }).format;
 
     const chartOptions = {
       // layout: {
@@ -51,9 +51,9 @@ const useCandleChart = (id: string, currency: Currency, data: ReturnType<typeof 
     const chart = createChart(document.getElementById(id)!, chartOptions);
 
     chart.applyOptions({
-      localization: {
-        priceFormatter: myPriceFormatter
-      },
+      // localization: {
+      //   priceFormatter: myPriceFormatter
+      // },
       // crosshair: {
       //   mode: CrosshairMode.Normal,
       //   vertLine: {
@@ -69,11 +69,45 @@ const useCandleChart = (id: string, currency: Currency, data: ReturnType<typeof 
     });
 
     const mainSeries = chart.addCandlestickSeries({
-      lastValueVisible: false
+      lastValueVisible: false,
     });
     mainSeries.setData(data as any);
 
+    const volumeData = data.map(d => ({ time: d.time, value: d.volume }));
+    const volumeSeries = chart.addHistogramSeries({
+      priceScaleId: 'volume',
+      lastValueVisible: false,
+      // color: 'rgba(100, 100, 100, 0.3)',
+      // priceFormat: {
+      //   type: 'volume'
+      // }
+    });
+    volumeSeries.setData(volumeData as any);
+
+    chart.priceScale('volume').applyOptions({
+      scaleMargins: {
+        top: 0.7,
+        bottom: 0
+      }
+    });
+
     chart.timeScale().fitContent();
+
+    let lastVolumePriceLine: IPriceLine | null = null;
+    chart.subscribeCrosshairMove((param) => {
+      if (!param || !param.time) return;
+
+      const volume = param.seriesData.get(volumeSeries);
+
+      if (lastVolumePriceLine) {
+        volumeSeries.removePriceLine(lastVolumePriceLine);
+      }
+
+      if (volume) {
+        lastVolumePriceLine = volumeSeries.createPriceLine({ price: (volume as any).value });
+      }
+
+    });
 
     return () => chart.remove();
   }, [document]);
@@ -86,7 +120,6 @@ const Candles = () => {
   useCandleChart('container', instrument?.split('_')[1] as Currency, candles);
 
   return <div id='container' un-h='96' >
-    hello
   </div>;
 };
 
