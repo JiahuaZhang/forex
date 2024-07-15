e46a43beee58c69faa1a3936a708852b5b379da2
Jiahua Zhang <hughzhanghz@gmail.com>
2024-07-15 19:03:50 -0400

    add pending order

--- app/.server/oanda/order.ts
+++ app/.server/oanda/order.ts
@@ -1,4 +1,5 @@
 import { oandaUrl } from './account';
+import { AccountID } from './type/account';
 import { GuaranteedStopLossOrderRequest, LimitOrderRequest, MarketIfTouchedOrderRequest, MarketOrderRequest, Order, OrderID, OrderStateFilter, StopLossOrderRequest, StopOrderRequest, TakeProfitOrderRequest, TrailingStopLossOrderRequest } from './type/order';
 import { AcceptDatetimeFormat, InstrumentName } from './type/primitives';
 import { OrderCancelTransaction, OrderFillTransaction, Transaction, TransactionID } from './type/transaction';
@@ -44,6 +45,16 @@ export const createOrder = async (id: string, order: MarketOrderRequest | LimitO
 
 export const getOrders = async ({ id, acceptDatetimeFormat, ids, state, instrument, count, beforeID }: { id: string; acceptDatetimeFormat?: AcceptDatetimeFormat, ids?: OrderID[], state?: OrderStateFilter, instrument?: InstrumentName, count?: number, beforeID?: OrderID; }) => {
   const response = await fetch(`${oandaUrl}/v3/accounts/${id}/orders`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.OANDA_API_KEY ?? ''}`,
+    },
+  });
+
+  return await response.json() as { orders: Order[]; lastTransactionID: TransactionID; };
+};
+
+export const getPendingOrders = async ({ id, acceptDatetimeFormat, accountID }: { id: string; acceptDatetimeFormat?: AcceptDatetimeFormat; accountID?: AccountID; }) => {
+  const response = await fetch(`${oandaUrl}/v3/accounts/${id}/pendingOrders`, {
     headers: {
       'Authorization': `Bearer ${process.env.OANDA_API_KEY ?? ''}`,
     },
---
+++ app/routes/oanda.order.$id_.pending.tsx
@@ -0,0 +1,37 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Descriptions, Popover } from 'antd';
+import { useLoaderData } from 'react-router';
+import { getPendingOrders } from '~/.server/oanda/order';
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  return getPendingOrders({ id: params.id! });
+};
+
+const Orders = () => {
+  const data = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  console.log(data);
+
+  return <div>
+    {
+      data.orders.map(order => <Descriptions bordered size='small' un-m='2'>
+        <Descriptions.Item label='id' >
+          <Popover content={<pre un-overflow-y='auto' >
+            {JSON.stringify(order)}
+          </pre>} >
+            {order.id}
+          </Popover>
+        </Descriptions.Item>
+        <Descriptions.Item label='time' >{new Date(order.createTime).toLocaleString()}</Descriptions.Item>
+        <Descriptions.Item label='type' >{order.type}</Descriptions.Item>
+        <Descriptions.Item label='trade ID' >{order.tradeID}</Descriptions.Item>
+        <Descriptions.Item label='price' >{order.price}</Descriptions.Item>
+        <Descriptions.Item label='time in force' >{order.timeInForce}</Descriptions.Item>
+        <Descriptions.Item label='trigger condition' >{order.triggerCondition}</Descriptions.Item>
+        <Descriptions.Item label='trigger mode' >{order.triggerMode}</Descriptions.Item>
+        <Descriptions.Item label='state' >{order.state}</Descriptions.Item>
+      </Descriptions>)
+    }
+  </div>;
+};
+
+export default Orders;
