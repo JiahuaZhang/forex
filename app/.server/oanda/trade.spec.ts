098aa215978a9d7475fd6f1378615a1211268fce
Jiahua Zhang <hughzhanghz@gmail.com>
2024-07-25 21:08:59 -0400

    update remix

--- app/.server/oanda/trade.spec.ts
+++ app/.server/oanda/trade.spec.ts
@@ -1,9 +1,27 @@
 import { test } from 'vitest';
 import { closeTrade } from './trade';
+import { MarketOrderRequest } from './type/order';
+import { createOrder } from './order';
 
 const ID = '101-001-27063409-001';
 
 test('close trade', async () => {
   const response = await closeTrade({ accountID: ID, tradeSpecifier: '255' });
   console.log(response);
+});
+
+test('close partial trade', async () => {
+  // const order: MarketOrderRequest = {
+  //   instrument: 'USD_CNH',
+  //   units: '100',
+  //   timeInForce: 'FOK',
+  //   type: 'MARKET',
+  //   positionFill: 'DEFAULT'
+  // };
+
+  // const orderResponse = await createOrder(ID, order);
+  // console.log(orderResponse.orderFillTransaction.id);
+
+  const closeResponse = await closeTrade({ accountID: ID, tradeSpecifier: '299', units: '5' });
+  console.log(closeResponse);
 });
